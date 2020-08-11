460fe8ce0707db318a3b6ccb911bcee3501756d7
Blouk <nicolas@blouk.com>
2020-08-11 15:01:52 +0200

    [ADD] website

---
+++ ep_dg_website/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- encoding: utf-8 -*-
+{
+    'name': 'EP DG Comm Website',
+    'category': 'Theme/EP_DG_Comm',
+    'summary': 'EP DG Comm Website',
+    'version': '0.1',
+    'description': '',
+    'depends': [
+        'website',
+        'website_event',
+    ],
+
+    'data': [
+
+        # data
+
+        # assets
+        'views/assets.xml',
+
+        # layout
+        'views/layout.xml',
+
+        #backend
+        'views/backend/website_menu_tree.xml',
+
+        # pages
+
+        # generic views
+
+    ],
+}
